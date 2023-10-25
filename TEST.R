353b939330d68d4d85b86e54d2e8865dc475df0d
siwei <vl199967@gmail.com>
2023-10-25 13:26:39 +0100

    fucking these hoes in a jet

--- TEST.R
+++ TEST.R
@@ -1,1 +1,12 @@
 ## USING GIT LMAO 
+## we didn't do wk3 lab so just pretend the code chunks output results lol
+```{r}
+library(terra)
+library(here)
+jan<-terra::rast(here("prac3_data", "wc2.1_5m_tavg_01.tif"))
+# have a look at the raster layer jan
+plot(jan)
+```
+fucking these hoes in a jet 
+```{r}
+```
